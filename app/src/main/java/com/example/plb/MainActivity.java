99a3cca9dc27f1ad5da190d399c0e9b977c6a763
zhc <zhc@qq.com>
2018-12-27 15:06:48 +0800

    界面框架 zhc

--- app/src/main/java/com/example/plb/MainActivity.java
+++ app/src/main/java/com/example/plb/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
         View view = mInflater.inflate(R.layout.tab_indicator,null);
         ImageView img = view.findViewById(R.id.icon_tab);
         TextView text = view.findViewById(R.id.txt_indicator);
+
         img.setBackgroundResource(tab.getImg());
         text.setText(tab.getTitle());
         return view;
