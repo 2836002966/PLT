895ac41dbacf43a053b8bf6db643fde9541912e2
zhc <zhc@qq.com>
2018-12-27 15:36:24 +0800

    引导页 zhc

--- app/src/main/java/com/example/plb/GuideActivity.java
+++ app/src/main/java/com/example/plb/GuideActivity.java
@@ -1,37 +1,28 @@
 package com.example.plb;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.WindowManager;
 
 public class GuideActivity extends AppCompatActivity {
 
-    private boolean isFirst;
     private Handler handler;
-    private SharedPreferences sp;
-    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_guide);
         handler = new Handler();
-        sp = getSharedPreferences("guide",MODE_PRIVATE);
-        editor = sp.edit();
-        sp.getBoolean("isFirst",true);
-        if (isFirst){
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    editor.putBoolean("isFirst",false).apply();
-                    startActivity(new Intent(GuideActivity.this,MainActivity.class));
-                }
-            },3000);
-        }else {
-            startActivity(new Intent(GuideActivity.this,MainActivity.class));
-        }
-
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(GuideActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
     }
 }
