88981a46642af37031bbd2bd53d98521df4b3094
zhc <zhc@qq.com>
2019-01-02 14:11:43 +0800

    购物车界面1 zhc

--- app/src/main/java/com/example/plb/MainActivity.java
+++ app/src/main/java/com/example/plb/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.plb;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -28,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //得到当前界面的装饰视图
+        if(Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //设置状态栏颜色为透明
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+        //隐藏标题栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_main);
         initTab();
     }
--- app/src/main/java/com/example/plb/fragment/ShopCartFragment.java
+++ app/src/main/java/com/example/plb/fragment/ShopCartFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.plb.R;
 
@@ -14,14 +17,63 @@ import com.example.plb.R;
  * Created by zhc on 2018/12/27.
  * 购物车
  */
-public class ShopCartFragment extends Fragment {
+public class ShopCartFragment extends Fragment implements View.OnClickListener{
 
     private View view;
+    private TextView manager;
+    private TextView total;
+    private TextView money;
+    private Button settlement;
+    private Button delete;
+    private boolean mark = true;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_shopcart,null);
+        initView();
         return view;
+    }
+
+    private void initView() {
+        manager = view.findViewById(R.id.manager);
+        manager.setOnClickListener(this);
+        total = view.findViewById(R.id.total);
+        money = view.findViewById(R.id.money);
+        settlement = view.findViewById(R.id.settlement);
+        settlement.setOnClickListener(this);
+        delete = view.findViewById(R.id.delete);
+        delete.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.manager:
+                if (mark){
+                    mark = false;
+                    manager.setText(R.string.complete);
+                    settlement.setVisibility(View.GONE);
+                    total.setVisibility(View.GONE);
+                    money.setVisibility(View.GONE);
+                    delete.setVisibility(View.VISIBLE);
+                }else {
+                    mark = true;
+                    manager.setText(R.string.manager);
+                    settlement.setVisibility(View.VISIBLE);
+                    total.setVisibility(View.VISIBLE);
+                    money.setVisibility(View.VISIBLE);
+                    delete.setVisibility(View.GONE);
+                }
+                break;
+            case R.id.settlement:
+                Toast.makeText(getContext(),"您还没有选择商品哦！",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(getContext(),"您还没有选择商品哦！",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
