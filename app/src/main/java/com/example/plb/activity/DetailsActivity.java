61b64a59be56559a7dabadac51e09a99311ff3ce
ckr2337871271 <2337871271@qq.com>
2019-01-12 10:45:44 +0800

    改动了状态栏

--- app/src/main/java/com/example/plb/activity/DetailsActivity.java
+++ app/src/main/java/com/example/plb/activity/DetailsActivity.java
@@ -28,16 +28,13 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
         //得到当前界面的装饰视图
         if(Build.VERSION.SDK_INT >= 21) {
             View decorView = getWindow().getDecorView();
-            //让应用主题内容占用系统状态栏的空间,注意:下面两个参数必须一起使用 stable 牢固的
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            decorView.setSystemUiVisibility(option);
             //设置状态栏颜色为透明
-            getWindow().setStatusBarColor( Color.TRANSPARENT);
+            getWindow().setStatusBarColor(getResources ().getColor (R.color.orange ));
         }
-        super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_details );
         textView1 = findViewById ( R.id.tvOne );
         textView2 = findViewById ( R.id.tvTwo );
